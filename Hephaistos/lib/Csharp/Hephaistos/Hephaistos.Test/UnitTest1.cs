e927743ec8db2c4d50f496ae501cd3cc6d1d7719
Christian <it@cj-mayer.de>
2020-06-14 18:48:10 +0200

    adjust icons, add empty Projects for client APIs

---
+++ Hephaistos/lib/Csharp/Hephaistos/Hephaistos.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Hephaistos.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Hephaistos/lib/Csharp/Hephaistos/Hephaistos/Hephaistos.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Hephaistos
+{
+    public class Hephaistos
+    {
+    }
+}
