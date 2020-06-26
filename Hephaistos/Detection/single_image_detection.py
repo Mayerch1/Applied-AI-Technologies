9a6a955cea0e030026b847d204768e67266fcd19
Daniel Haerer <Daniel.Haerer@icloud.com>
2020-06-26 17:35:41 +0200

    fixed new input size

--- Hephaistos/Detection/single_image_detection.py
+++ Hephaistos/Detection/single_image_detection.py
@@ -107,7 +107,7 @@ class hooks:
         if facedetect=="ok": return 0
         elif facedetect=="nomask": return 1
         
-        picture=tf.keras.preprocessing.image.load_img(os.path.dirname(__file__) + "/" + data, grayscale=False, color_mode="rgb", target_size=(128,128), interpolation="nearest")
+        picture=tf.keras.preprocessing.image.load_img(os.path.dirname(__file__) + "/" + data, grayscale=False, color_mode="rgb", target_size=(256,256), interpolation="nearest")
         array = tf.keras.preprocessing.image.img_to_array(picture)
         array = np.array([array])  # Convert single image to a batch.
         predict = loaded_model.predict(array)[0]
