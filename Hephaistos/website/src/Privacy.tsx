d064b9ca1fe749d739a4c0e998125b82c0cf5ca1
Christian <it@cj-mayer.de>
2020-07-06 12:47:27 +0200

    change email

--- Hephaistos/website/src/Privacy.tsx
+++ Hephaistos/website/src/Privacy.tsx
@@ -15,7 +15,7 @@ class Privacy extends React.Component {
 
         <h1>Privacy Policy for Hephaistos</h1>
         <p>At Hephaistos, accessible from www.hephaistos.online, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Hephaistos and how we use it.</p>
-        <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us (info@hephaistos.online or https://github.com/Mayerch1/Applied-AI-Technologies). </p>
+        <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us (privacy(at)hephaistos(dot)online or https://github.com/Mayerch1/Applied-AI-Technologies). </p>
         
         <h2>Uploaded Images</h2>
         <p>Hephaistos stores images uploaded by the user.
