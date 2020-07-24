1e2946834cc970370bdf3812b84e8a5cab5fbcaf
Gabriel Goldschmitt <G.Goldschmitt@gmx.de>
2020-07-24 16:37:32 +0200

    Final build

---
+++ Hephaistos/website/build/precache-manifest.2809308ed8b1d8ee333d73622c2888e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e573996b061f3ab1b867083e01eb57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06ef4bcb708f4494c262",
+    "url": "/static/css/main.75a5dedf.chunk.css"
+  },
+  {
+    "revision": "09e1fd6afa42045d960a",
+    "url": "/static/js/2.c14f365f.chunk.js"
+  },
+  {
+    "revision": "c12e0736d7b338bc8e919116e59d56c6",
+    "url": "/static/js/2.c14f365f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ef4bcb708f4494c262",
+    "url": "/static/js/main.9d270ee4.chunk.js"
+  },
+  {
+    "revision": "1f168ef2b05bd633138a",
+    "url": "/static/js/runtime-main.e77f80eb.js"
+  },
+  {
+    "revision": "45a63ef1c37ef6ea09b902dd547b4d9c",
+    "url": "/static/media/logo.45a63ef1.png"
+  },
+  {
+    "revision": "e82b113ea8772bc785a042fb03dade8f",
+    "url": "/static/media/logo_small.e82b113e.png"
+  },
+  {
+    "revision": "87acb9c731d5f5f1b8d9abea96ec270a",
+    "url": "/static/media/passed.87acb9c7.png"
+  }
+]);
